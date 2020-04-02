141d424c395dfb27f2e6f9de8fa8534e1b482e94
Matheus Régis <matheustrai@gmail.com>
2020-04-01 22:52:28 -0300

    Create User Model

---
+++ backend/src/app/models/User.js
@@ -0,0 +1,32 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        cpf: Sequelize.STRING,
+        birthday: Sequelize.DATE,
+        address: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+    this.addHook('beforeSave', async (user) =>{
+      console.log(user.password_hash);
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+      console.log(user.password_hash);
+    });
+
+    return this;
+  }
+}
+
+export default User;
